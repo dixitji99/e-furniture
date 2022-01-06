8d0efff88956359966a4e443ab4527321516a7d3
Ayush <dixitji99@gmail.com>
2022-01-06 16:40:04 +0530

    fix(navbar): navbar made responsive

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,30 +1,35 @@
 import './Navbar.scss'
-import {Link} from 'react-router-dom';
+import Sidebar from './Sidebar'
+import { Link } from 'react-router-dom';
+
 
 function Navbar() {
+
+
     return (
         <>
+
             <div className="navbar">
                 <div className='navbar__logo-name'>
                     Furni
                 </div>
+                <div className="navbar__side-navbar">
+                    <Sidebar />
+                </div>
+                                
                 <div className="navbar__nav-links">
                     <ul>
-                        {/* <li>Home</li>
-                        <li>About us</li>
-                        <li>Furniture</li>
-                        <li>Blog</li>
-                        <li>Contact us</li> */}
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/'>About us</Link></li>
-                        <li><Link to='/'>Furniture</Link></li>
-                        <li><Link to='/'>Blog</Link></li>
-                        <li><Link to='/'>Contact us</Link></li>
+                        <li><Link to='e-furniture/'>Home</Link></li>
+                        <li><Link to='about/'>About us</Link></li>
+                        <li><Link to='furniture/'>Furniture</Link></li>
+                        <li><Link to='blog/'>Blog</Link></li>
+                        <li><Link to='contact_us/'>Contact us</Link></li>
                     </ul>
                 </div>
                 <div className="navbar__login-button">
-                    <button className='navbar__login-button--round-button'>Login</button>
+                    <Link to='login_page/'><button className='navbar__login-button--round-button'>Login</button></Link>
                 </div>
+
             </div>
         </>
     )
